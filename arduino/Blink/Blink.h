0d6f26b4f0d696fedfa7cd80aa1bcb0246e7ec48
cwbollinger <cwbollinger@gmail.com>
2012-04-09 00:32:47 +0000

    Initial import.

---
+++ arduino/Blink/Blink.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef Blink_H_
+#define Blink_H_
+#include "Arduino.h"
+//add your includes for the project Blink here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project Blink here
+
+
+
+
+//Do not add code below this line
+#endif /* Blink_H_ */
