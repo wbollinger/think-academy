e0f9095e7bcdd1ac10a3c0a45a9c57320319b3ac
cwbollinger <cwbollinger@gmail.com>
2012-04-16 23:43:22 +0000

    Milestone:  140 points?

--- rescue2012/src/rescue/StateLineFollow.java
+++ rescue2012/src/rescue/StateLineFollow.java
@@ -6,6 +6,10 @@ public class StateLineFollow extends State {
 	private static StateLineFollow instance = new StateLineFollow();
 
 	// Kc = 4 Pc = .25 dT = .0028
+	private double KpLocal = 8.00; // 10.00
+	private double KiLocal = 0.70; // 0.70
+	private double KdLocal = 6.00; // 6.00
+	
 	public double Kp = 8.00; // 10.00
 	public double Ki = 0.70; // 0.70
 	public double Kd = 6.00; // 6.00
@@ -34,6 +38,7 @@ public class StateLineFollow extends State {
 
 	public void enter(Robot robot) {
 		//debugln("StLineFollow enter");
+		robot.dropCompass();
 	}
 
 	public void execute(Robot robot) {
@@ -50,21 +55,50 @@ public class StateLineFollow extends State {
 		int powerLeft = 0;
 
 		int n = 0;
+		int rampCount = 0;
+		
+		int accelAverage = 0;
 
 		robot.motRight.forward();
 		robot.motLeft.forward();
 
 		while (!Button.ESCAPE.isDown()) {
-			if(n < 5) {
-				if (robot.accel.getXAccel() > 50) {
-					if (robot.getBaseMotorPower() != 90) {
-						robot.setBaseMotorPower(90);
+			if(n < 7) {
+				accelAverage += robot.accel.getXAccel();
+				accelAverage /= 2;
+				//debugln(""+accelAverage);
+				if (accelAverage > 40) {
+					if(rampCount < 5) {
+						rampCount++;
 					}
 				} else {
-					if (robot.getBaseMotorPower() != 40) {
-						robot.setBaseMotorPower(40);
+					if(rampCount > -7) {
+						rampCount--;
 					}
 				}
+				
+				if (rampCount >= 2){
+					robot.isOnRamp = true;
+				} else if (rampCount <= 0) {
+					robot.isOnRamp = false;
+				}
+
+				if (robot.isOnRamp){
+					if (robot.getBaseMotorPower() != 70) {
+						robot.setBaseMotorPower(70);	
+					}
+					KpLocal = 5.00; // 10.00
+					KiLocal = 0.00; // 0.70
+					KdLocal = 0.00; // 6.00
+				} else {
+					if (robot.getBaseMotorPower() != 45) {
+						robot.setBaseMotorPower(45);
+					}
+					KpLocal = Kp; // 10.00
+					KiLocal = Ki; // 0.70
+					KdLocal = Kd; // 6.00
+				}
+				
 				n = 0;
 			}
 			
@@ -80,7 +114,7 @@ public class StateLineFollow extends State {
 			derivative = error - lastError;
 			// debugln("error: " + error + " integral: " + integral +
 			// " derivative: " + derivative);
-			turn = (int) Util.round(Kp * error + Ki * integral + Kd * derivative);
+			turn = (int) Util.round(KpLocal * error + KiLocal * integral + KdLocal * derivative);
 			// if(Turn == 0) {
 			// debugln("I'm driving straight");
 			// robot.forward(2);
@@ -96,14 +130,17 @@ public class StateLineFollow extends State {
 
 			if (robot.ultrasonic.getDistance() < 10) {
 				robot.changeState(StateAvoidObstacle.getInstance());
+				//robot.changeState(StateCommand.getInstance());
 				return;
 			}
-			 if ((robot.getLightLeft() > 65)||(robot.getLightRight() > 65)) {
-			 Sound.playTone(440, 100);
-			 robot.stop();
-			 robot.changeState(StateCommand.getInstance());
-			 return;
-			 }
+			if ((robot.getLightLeft() > robot.threshSilver)||(robot.getLightRight() > robot.threshSilver)) {
+				Sound.playTone(440, 100);
+				robot.stop();
+				robot.backward(2);
+				robot.liftCompass();
+				robot.changeState(StateCommand.getInstance());
+				return;
+			}
 			if (Button.ENTER.isDown()) {
 				robot.changeState(StateCommand.getInstance());
 				return;
