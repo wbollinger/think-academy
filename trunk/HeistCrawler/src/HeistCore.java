04dd78b51833b0578efa9f4f3382ea47b09ce61b
cwbollinger@gmail.com <cwbollinger@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2012-05-19 15:35:41 +0000

    Added inventory panel: no functionality yet

--- trunk/HeistCrawler/src/HeistCore.java
+++ trunk/HeistCrawler/src/HeistCore.java
@@ -2,14 +2,24 @@ import javax.swing.*;
 
 public class HeistCore extends JFrame {
 	
-	public static int width = 800;
-	public static int height = 600;
+	public static int width = HeistPanel.width;
+	public static int height = HeistPanel.height+InventoryPanel.height;
+	
 	public static HeistCore mainClass;
+	
+	public static HeistPanel mainPane;
+	public static InventoryPanel inventoryPane;
 	
 	public HeistCore() {
 		setSize(width, height);
+		setLayout(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		add(new HeistPanel());
+		mainPane = new HeistPanel();
+		inventoryPane = new InventoryPanel();
+		add(mainPane);
+		add(inventoryPane);
+		mainPane.setBounds(0, 0, HeistPanel.width, HeistPanel.height);
+		inventoryPane.setBounds(0, HeistPanel.height, InventoryPanel.width, InventoryPanel.height);
 		setResizable(false);
 		setTitle("THE HEIST!");
 		
