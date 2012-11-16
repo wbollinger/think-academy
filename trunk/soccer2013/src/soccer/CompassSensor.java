ec96eeabf45af565ede6694c4568fd9c66a12c26
boneman09@gmail.com <boneman09@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2012-11-16 04:03:10 +0000

    New class for the compass sensor turn tests.

---
+++ trunk/soccer2013/src/soccer/CompassSensor.java
@@ -0,0 +1,87 @@
+package soccer;
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.CompassHTSensor;
+
+public class CompassSensor {
+
+	NXTMotor motA;
+	NXTMotor motB;
+	CompassHTSensor compass;
+
+	CompassSensor() {
+
+		compass = new CompassHTSensor(SensorPort.S2);
+		motA = new NXTMotor(MotorPort.A);
+		motB = new NXTMotor(MotorPort.B);
+
+	}
+	
+//Just what I thought was the old compass averaging code
+	/*
+	 * public float getHeading() { float cReading = 0.0f; float
+	 * correctedCReading = 0.0f;
+	 * 
+	 * if (compass != null) { cReading = compass.getDegrees(); }
+	 * correctedCReading = 360 - cReading + 90; // debugln("Old Heading: " +
+	 * correctedCReading); if (correctedCReading >= 360) { correctedCReading =
+	 * correctedCReading - 360; } correctedCReading = correctedCReading -
+	 * compOffset; // debugln("New Heading: " + correctedCReading); if
+	 * (correctedCReading < 0) { correctedCReading = correctedCReading + 360; }
+	 * return correctedCReading; }
+	 */
+
+	public void degreeTurn(int n) {
+		int degree = (int) compass.getDegrees();
+		int newDegree = degree + n;
+		if (newDegree > 359) {
+			newDegree = newDegree - 360;
+		}
+		// LCD.drawString("New Degree:" + newDegree, 0, 2);
+		if (n < 180) {
+			while (true) {
+				// turnRight();
+				if (compass.getDegrees() == newDegree) {
+					break;
+				}
+			}
+		} else {
+			while (true) {
+				// turnLeft();
+				if (compass.getDegrees() == newDegree) {
+					break;
+				}
+			}
+		}
+
+		// allStop();
+
+	}
+
+	public void run() throws InterruptedException {
+
+		while (Button.ESCAPE.isUp()) {
+
+			LCD.drawInt((int) compass.getDegrees(), 0, 1);
+			degreeTurn(90);
+			Thread.sleep(20);
+			LCD.clear();
+
+		}
+	}
+
+	/**
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws InterruptedException {
+
+		CompassSensor bot = new CompassSensor();
+		bot.run();
+	}
+
+}
