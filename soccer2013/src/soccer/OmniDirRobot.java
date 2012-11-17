26df5ce4066252ca9e2da00533d8ee4ec2ea1dd0
cwbollinger <cwbollinger@gmail.com>
2012-11-17 00:43:52 +0000

    mods to OmniDirRobot and CompassSensor

--- soccer2013/src/soccer/OmniDirRobot.java
+++ soccer2013/src/soccer/OmniDirRobot.java
@@ -6,19 +6,27 @@ import lejos.nxt.addon.IRSeekerV2;
 
 public class OmniDirRobot {
 
-	NXTMotor motA = new NXTMotor(MotorPort.A);
-	NXTMotor motB = new NXTMotor(MotorPort.B);
-	NXTMotor motC = new NXTMotor(MotorPort.C);
-	IRSeekerV2 IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
-
-	public void run() throws InterruptedException {
+	NXTMotor motA;
+	NXTMotor motB;
+	NXTMotor motC;
+	IRSeekerV2 IR;
+	
+	public OmniDirRobot() {
+		motA = new NXTMotor(MotorPort.A);
+		motB = new NXTMotor(MotorPort.B);
+		motC = new NXTMotor(MotorPort.C);
+		IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
+		
 		motA.setPower(50);
 		motB.setPower(50);
 		motC.setPower(50);
 		motA.stop();
 		motB.stop();
 		motC.stop();
+	}
 
+	public void run() throws InterruptedException {
+		
 		while (true) {
 			
 			if(IR.getDirection() > 0 && IR.getDirection() < 5){
