5e9d2197c7159ecbd97c0268ed616bc7a7932a7a
cwbollinger@gmail.com <cwbollinger@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2012-11-21 01:57:55 +0000

    Changes to Robot and RobotChris

--- trunk/soccer2013/src/soccer/RobotChris.java
+++ trunk/soccer2013/src/soccer/RobotChris.java
@@ -1,6 +1,8 @@
 package soccer;
 
+import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
+import lejos.nxt.SensorPort;
 import lejos.nxt.addon.IRSeekerV2;
 
 public class RobotChris extends Robot{
@@ -10,5 +12,56 @@ public class RobotChris extends Robot{
 	NXTMotor motC;
 	IRSeekerV2 IR;
 	
+	public RobotChris() {
+		super();
+		motA = new NXTMotor(MotorPort.A);
+		motB = new NXTMotor(MotorPort.B);
+		motC = new NXTMotor(MotorPort.C);
+		IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
+		
+		motA.setPower(50);
+		motB.setPower(50);
+		motC.setPower(50);
+		motA.stop();
+		motB.stop();
+		motC.stop();
+	}
 	
+	public void run() {
+		
+	}
+	
+	public void moveForward(){
+		
+		motC.backward();
+		motB.forward();
+		
+	}
+	
+	public void stopAll(){
+		motA.stop();
+		motB.stop();
+		motC.stop();
+	}
+
+	public void turnLeft() {
+
+		motA.forward();
+		motB.forward();
+		motC.forward();
+
+	}
+
+	public void turnRight() {
+
+		motA.backward();
+		motB.backward();
+		motC.backward();
+
+	}
+	
+	public static void main(String[] args) {
+		Robot robot = RobotFactory.makeRobot();
+		robot.run();
+	}
 }
