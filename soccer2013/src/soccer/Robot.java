9c8b055715405f8ee65c6d754503f2b1a7588283
cwbollinger <cwbollinger@gmail.com>
2012-11-29 01:58:40 +0000

    Integrated HolonomicTest into Navigator

--- soccer2013/src/soccer/Robot.java
+++ soccer2013/src/soccer/Robot.java
@@ -7,6 +7,13 @@ import lejos.nxt.addon.IRSeekerV2;
 
 public class Robot {
 
+	protected static double r;
+	protected static double b;
+	
+	public final static Vector2D F0 = new Vector2D(-1.0, 0.0);
+	public final static Vector2D F1 = new Vector2D(0.5, -1.0*Math.sqrt(3)/2.0);
+	public final static Vector2D F2 = new Vector2D(0.5, Math.sqrt(3)/2.0);
+	
 	NXTMotor motA;
 	NXTMotor motB;
 	NXTMotor motC;
@@ -25,16 +32,7 @@ public class Robot {
 	}
 	
 	public Robot() {
-		motA = new NXTMotor(MotorPort.A);
-		motB = new NXTMotor(MotorPort.B);
-		motC = new NXTMotor(MotorPort.C);
-		
-		motA.setPower(50);
-		motB.setPower(50);
-		motC.setPower(50);
-		motA.stop();
-		motB.stop();
-		motC.stop();
+
 	}
 	
 	public void sleep(int time) {
@@ -46,29 +44,22 @@ public class Robot {
 		}
 	}
 	
+	public static double getR() {
+		return r;
+	}
+
+	public static double getB() {
+		return b;
+	}
+	
 	public void followBall(){}
 	
-	public void moveForward(){
-		motC.forward();
-		motB.backward();
-	}
+	public void moveForward(){}
 	
-	public void stopAll(){
-		motA.stop();
-		motB.stop();
-		motC.stop();
-	}
+	public void stopAll(){}
 
-	public void turnLeft() {
-		motA.backward();
-		motB.backward();
-		motC.backward();
-	}
+	public void turnLeft() {}
 
-	public void turnRight() {
-		motA.forward();
-		motB.forward();
-		motC.forward();
-	}
+	public void turnRight() {}
 
 }
