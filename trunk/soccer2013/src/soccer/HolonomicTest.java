759851e042721f72a0b1cdc2e4f43bad2acbd832
cwbollinger@gmail.com <cwbollinger@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2012-11-29 01:29:55 +0000

    Changes to HolonomicTest

--- trunk/soccer2013/src/soccer/HolonomicTest.java
+++ trunk/soccer2013/src/soccer/HolonomicTest.java
@@ -11,145 +11,42 @@ public class HolonomicTest {
 	public final static Vector2D F1 = new Vector2D(0.5, -1.0*Math.sqrt(3)/2.0);
 	public final static Vector2D F2 = new Vector2D(0.5, Math.sqrt(3)/2.0);
 	
-	NXTMotor motA;
-	NXTMotor motB; 
-	NXTMotor motC;
+	Robot robot;
 		
 	public HolonomicTest() {
-		motA = new NXTMotor(MotorPort.A);
-		motB = new NXTMotor(MotorPort.B);
-		motC = new NXTMotor(MotorPort.C);
-		motA.setPower(100);
-		motB.setPower(100);
-		motC.setPower(100);
+		robot = Robot.getRobot();
 	}
 	
 	public void forward() {
-		motA.backward();
-		motB.forward();
-		motC.stop();
+		robot.motA.backward();
+		robot.motB.forward();
+		robot.motC.stop();
 	}
 	
 	public void forward(int time) {
-		motA.backward();
-		motB.forward();
-		motC.stop();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
-	}
-	
-	public void forwardLeft() {
-		motA.stop();
-		motB.backward();
-		motC.forward();
-	}
-	
-	public void forwardLeft(int time) {
-		motA.stop();
-		motB.backward();
-		motC.forward();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
-	}
-	
-	public void forwardRight() {
-		motA.forward();
-		motB.stop();
-		motC.backward();
-	}
-	
-	public void forwardRight(int time) {
-		motA.forward();
-		motB.stop();
-		motC.backward();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
+		robot.motA.backward();
+		robot.motB.forward();
+		robot.motC.stop();
+		robot.sleep(time);
+		robot.motA.stop();
+		robot.motB.stop();
+		robot.motC.stop();
 	}
 	
 	public void backward() {
-		motA.forward();
-		motB.backward();
-		motC.stop();
+		robot.motA.forward();
+		robot.motB.backward();
+		robot.motC.stop();
 	}
 	
 	public void backward(int time) {
-		motA.forward();
-		motB.backward();
-		motC.stop();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
-	}
-	
-	public void backwardRight() {
-		motA.stop();
-		motB.forward();
-		motC.backward();
-	}
-	
-	public void backwardRight(int time) {
-		motA.stop();
-		motB.forward();
-		motC.backward();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
-	}
-	
-	public void backwardLeft() {
-		motA.backward();
-		motB.stop();
-		motC.forward();
-	}
-	
-	public void backwardLeft(int time) {
-		motA.backward();
-		motB.stop();
-		motC.forward();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
-	}
-	
-	public void turnLeft() {
-		motA.forward();
-		motB.forward();
-		motC.forward();
-	}
-	
-	public void turnLeft(int time) {
-		motA.forward();
-		motB.forward();
-		motC.forward();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
-	}
-	
-	public void turnRight() {
-		motA.backward();
-		motB.backward();
-		motC.backward();
-	}
-	
-	public void turnRight(int time) {
-		motA.backward();
-		motB.backward();
-		motC.backward();
-		sleep(time);
-		motA.stop();
-		motB.stop();
-		motC.stop();
+		robot.motA.forward();
+		robot.motB.backward();
+		robot.motC.stop();
+		robot.sleep(time);
+		robot.motA.stop();
+		robot.motB.stop();
+		robot.motC.stop();
 	}
 	
 	public void moveDir(double dir) {
@@ -174,28 +71,21 @@ public class HolonomicTest {
 		
 		LCD.drawString(Double.toString(scale), 0, 4);
 		
-		motA.setPower((int)Math.round(w0*scale));
-		motB.setPower((int)Math.round(w1*scale));
-		motC.setPower((int)Math.round(w2*scale));
-		motA.forward();
-		motB.forward();
-		motC.forward();
+		robot.motA.setPower((int)Math.round(w0*scale));
+		robot.motB.setPower((int)Math.round(w1*scale));
+		robot.motC.setPower((int)Math.round(w2*scale));
+		robot.motA.forward();
+		robot.motB.forward();
+		robot.motC.forward();
 	}
 	
 	public void run() {
 		moveDir(0);
-		sleep(5000);
-	}
-	
-	public void sleep(int time) {
-		try {
-			Thread.sleep(time);
-		} catch(Exception e) {
-			System.err.println(e);
-		}
+		robot.sleep(5000);
 	}
 		
 	public static void main(String[] args) throws InterruptedException{
+		
 		HolonomicTest test = new HolonomicTest();
 		test.run();
 	}
