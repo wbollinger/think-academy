ca789d5e8c9bcaed020bd330c3050884addfd621
cwbollinger <cwbollinger@gmail.com>
2012-12-12 02:32:01 +0000

    Updated Main to initialize bluetooth connection.

--- soccer2013/src/soccer/Main.java
+++ soccer2013/src/soccer/Main.java
@@ -8,33 +8,34 @@ public class Main {
 	public static int checkButtons() {
 		// uncomment to enable button check
 
-//		// wait for a button press from the user
-//		int buttons;
-//		while ((buttons = Button.readButtons()) == 0) {
-//		}
+		// wait for a button press from the user
+		int buttons;
+		while ((buttons = Button.readButtons()) == 0) {
+		}
 //		// wait until the button is released!
-//		while (Button.ENTER.isDown()) {
-//		}
-//		return buttons;
+		while (Button.ENTER.isDown()) {
+		}
+		return buttons;
 
 		// tell main() to run 
-		return Button.ID_RIGHT;
+		//return Button.ID_RIGHT;
 	}
 
 	public static void main(String[] args) {
-		int buttons = -1;
-
+		
+		int buttons = checkButtons();
+		
 		// create a robot
 		Robot robot = Robot.getRobot();
 
 		if (buttons == Button.ID_ENTER) {
 			// Run in autonomous mode
-			//robot.setUseCommands(false);
-			//robot.setUseDebug(false);
+			robot.io.setUseCommands(false);
+			robot.io.setUseDebug(false);
 		} else {
 			// Use BlueTooth command loop
-			//robot.setUseCommands(true);
-			//robot.setUseDebug(true);
+			robot.io.setUseCommands(true);
+			robot.io.setUseDebug(true);
 		}
 		//robot.followBall();
 		robot.run(); //uncomment to activate state machine
