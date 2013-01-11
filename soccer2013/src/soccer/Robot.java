0238b8779edb98e7cd27b98c45c3a0b7e698006b
cwbollinger <cwbollinger@gmail.com>
2013-01-11 04:31:06 +0000

    Reformatted some code, refactored some variables, and got followBall working for RobotChris. Also moved HTCompass sensor declaration to Robot. Still initialized in individual robot classes.

--- soccer2013/src/soccer/Robot.java
+++ soccer2013/src/soccer/Robot.java
@@ -5,53 +5,55 @@ import lejos.nxt.LightSensor;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.addon.CompassHTSensor;
 import lejos.nxt.addon.IRSeekerV2;
 
 public class Robot {
-	
+
 	State current_state;
 	boolean exit;
-	
+
 	public String name;
 
 	protected static double r;
 	protected static double b;
-	
+
 	public final static Vector2D F0 = new Vector2D(-1.0, 0.0);
-	public final static Vector2D F1 = new Vector2D(0.5, -1.0*Math.sqrt(3)/2.0);
-	public final static Vector2D F2 = new Vector2D(0.5, Math.sqrt(3)/2.0);
-	
-//	Navigator nav;
-	
+	public final static Vector2D F1 = new Vector2D(0.5, -1.0 * Math.sqrt(3) / 2.0);
+	public final static Vector2D F2 = new Vector2D(0.5, Math.sqrt(3) / 2.0);
+
+	// Navigator nav;
+
 	NXTMotor motA;
 	NXTMotor motB;
 	NXTMotor motC;
-	
+
 	BrickIO io;
-	
+
 	IRSeekerV2 IR;
-	
+	CompassHTSensor compass;
+
 	// this is a singleton
 	private static Robot robot;
-	
+
 	public static Robot getRobot() {
 		if (robot == null) {
 			robot = RobotFactory.makeRobot();
 		}
 		if (robot == null) {
-			
+
 		}
 		return robot;
 	}
-	
+
 	public Robot(String name) {
 		this.name = name;
 		current_state = StateStart.getInstance();
 		io = new BrickIO();
-//		nav = new Navigator();
+		// nav = new Navigator();
 		exit = false;
 	}
-	
+
 	public void sleep(int time) {
 		try {
 			Thread.sleep(time);
@@ -60,7 +62,7 @@ public class Robot {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static double getR() {
 		return r;
 	}
@@ -68,58 +70,58 @@ public class Robot {
 	public static double getB() {
 		return b;
 	}
-	
-	public void followBall(){}
-	
-	public void moveForward(){}
-	
 
-	public void moveBackward(){}
-	
+	public void followBall() {
+	}
 
-	public void moveForward(int time){}
-	
+	public void moveForward() {
+	}
 
-	public void stopAll(){}
+	public void moveBackward() {
+	}
 
-	public void turnLeft(){}
-	
-	public void turnLeft(int time) {}
+	public void moveForward(int time) {
+	}
 
-	public void turnRight(){}
-	
+	public void stopAll() {
+	}
+
+	public void turnLeft() {
+	}
+
+	public void turnLeft(int time) {
+	}
+
+	public void turnRight() {
+	}
+
+	public void turnRight(int time) {
+	}
+
 	public void run() {
 		while (Button.ESCAPE.isUp() && !this.exit) {
 			update();
 		}
 	}
-	
-	public void turnRight(int time) {}
-	
-	public boolean check(MotorPort mot){
-		if(mot.getTachoCount() == -1){
+
+	public boolean check(MotorPort mot) {
+		if (mot.getTachoCount() == -1) {
 			return false;
-		}else{
+		} else {
 			return true;
 		}
-		
+
 	}
-	
+
 	/*
-	public boolean check(SensorPort sen){
-		if(sen.i2cStatus() == -5){
-			LightSensor lit = new LightSensor(sen);
-			if(lit.getLightValue() == 0){
-				return false;
-			}else{
-				return true;
-			}
-		}else{
-			
-		}
-		
-	}
-*/
+	 * public boolean check(SensorPort sen){ if(sen.i2cStatus() == -5){
+	 * LightSensor lit = new LightSensor(sen); if(lit.getLightValue() == 0){
+	 * return false; }else{ return true; } }else{
+	 * 
+	 * }
+	 * 
+	 * }
+	 */
 	public void update() {
 		if (current_state != null) {
 			current_state.execute(this);
--- soccer2013/src/soccer/RobotJake.java
+++ soccer2013/src/soccer/RobotJake.java
@@ -6,29 +6,23 @@ import lejos.nxt.SensorPort;
 import lejos.nxt.addon.CompassHTSensor;
 import lejos.nxt.addon.IRSeekerV2;
 
-public class RobotJake extends Robot{
-	NXTMotor motA;
-	NXTMotor motB;
-	NXTMotor motC;
-	IRSeekerV2 IR;
-	CompassHTSensor compass;
-	
-	public RobotJake(String name){
-	super(name);
-		
-	motA = new NXTMotor(MotorPort.A);
-	motB = new NXTMotor(MotorPort.B);
-	motC = new NXTMotor(MotorPort.C);
-	IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
-	compass = new CompassHTSensor(SensorPort.S2);
-	
-	motA.setPower(50);
-	motB.setPower(50);
-	motC.setPower(50);
-	motA.stop();
-	motB.stop();
-	motC.stop();
-}
+public class RobotJake extends Robot {
 
+	public RobotJake(String name) {
+		super(name);
 
+		motA = new NXTMotor(MotorPort.A);
+		motB = new NXTMotor(MotorPort.B);
+		motC = new NXTMotor(MotorPort.C);
+		IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
+		compass = new CompassHTSensor(SensorPort.S2);
+
+		motA.setPower(50);
+		motB.setPower(50);
+		motC.setPower(50);
+		motA.stop();
+		motB.stop();
+		motC.stop();
 	}
+
+}
