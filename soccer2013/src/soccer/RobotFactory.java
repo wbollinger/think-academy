1cb15f1ebce49bb6e9ecc98c8ca1f2d439c05bb5
cwbollinger <cwbollinger@gmail.com>
2013-01-11 03:05:23 +0000

    Added ebay to robot factory

--- soccer2013/src/soccer/RobotFactory.java
+++ soccer2013/src/soccer/RobotFactory.java
@@ -10,23 +10,26 @@ public class RobotFactory {
 		Properties props = Settings.getProperties();
 		String name = props.getProperty("lejos.usb_name");
 		Robot robot;
-		if(name.equals("NXTChris")||name.equals("bbot")) { // Chris' robot
+		if (name.equals("NXTChris") || name.equals("bbot")) { // Chris' robot
 			robot = new RobotChris(name);
 			robot.name = name;
-		} else if(name.equals("JPNXT")) { // Jeremy's robot
+		} else if (name.equals("JPNXT")) { // Jeremy's robot
 			robot = new RobotJeremy(name);
 			robot.name = name;
-		} else if(name.equals("Tim")) { // Tim's robot
+		} else if (name.equals("Tim")) { // Tim's robot
 			robot = new RobotTim(name);
 			robot.name = name;
-		} else if(name.equals("LineBacker")) { // Jake's robot
+		} else if (name.equals("ebay")) { // Dead display, no sound
+			robot = new RobotEbay(name);
+			robot.name = name;
+		} else if (name.equals("LineBacker")) { // Jake's robot
 			robot = new RobotJake(name);
 			robot.name = name;
 		} else {
 			robot = new Robot(name);
 		}
-		
+
 		return robot;
 	}
-	
+
 }
