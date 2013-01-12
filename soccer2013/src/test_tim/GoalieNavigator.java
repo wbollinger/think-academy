b4e031e91bffc210c2132f61ad48c5682b12d15e
boneman09 <boneman09@gmail.com>
2013-01-12 01:10:59 +0000

    GoalieNavigator might be inverted

--- soccer2013/src/test_tim/GoalieNavigator.java
+++ soccer2013/src/test_tim/GoalieNavigator.java
@@ -10,55 +10,48 @@ public class GoalieNavigator extends Navigator {
 	
 	IRSeekerV2 IR;
 
-	public GoalieNavigator(Robot robot) {
-		super(robot);
+	public GoalieNavigator(Robot bot) {
+		super(bot);
 		IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
 
 	}
 
-	public void goalieDefault() {
-		while (true) {
-			moveDir(60);
-			bot.sleep(3000);
-			moveDir(240);
-			bot.sleep(3000);
-		}
-	}
+//	public void goalieDefault() {
+//		while (true) {
+//			moveDir(60);
+//			bot.sleep(3000);
+//			moveDir(240);
+//			bot.sleep(3000);
+//		}
+//	}
 
 	public void goalieAware() {
 		while (true) {
 
 			if (IR.getDirection() > 0 && IR.getDirection() < 5) {
-				if (IR.getSensorValue(3) > 200) {
-					bot.stopAll();
-				} else {
-					// This goes left
+				//turns left?
 					moveDir(240);
-				}
-				if (IR.getSensorValue(3) > 200) {
-					bot.stopAll();
-				}
 			}
 			if (IR.getDirection() == 5) {
 				bot.stopAll();
 			}
-
-			// if(IR.getAngle() > 350 || IR.getAngle() < 10){
-			bot.stopAll();
-			// }
 			if (IR.getDirection() > 5 && IR.getDirection() < 10) {
-
-				// This goes right
+				//turns right?
 				moveDir(60);
 			}
 
 		}
+	
 	}
 
 	public void run() {
 
 		goalieAware();
 
+	}
+	public void main(String[] args){
+		Robot robot = Robot.getRobot();
+		run();
 	}
 
 }
