4ec6a278cc35038dd2df240b2e61ac8bd60e58bc
Jthelegoman <jthelegoman@gmail.com>
2013-02-13 02:29:14 +0000

    Added stuff to StateStriker.

--- soccer2013/src/soccer/StateStriker.java
+++ soccer2013/src/soccer/StateStriker.java
@@ -4,12 +4,12 @@ public class StateStriker extends State{
 
 	private static StateStriker instance = new StateStriker();
 	Robot robot = Robot.getRobot();
-	Navigator nav = new Navigator(robot);
 
 	//--------------------------------------------------------------------------------------
 	public void execute(Robot robot) {
 		robot.followBall();
-		nav.pointToGoal();
+		robot.nav.pointToHeading(90);//number will be the north of the field.
+		robot.nav.pointToGoal();
 		//moveForward will be changed to kick. 
 		robot.moveForward();
 	}
