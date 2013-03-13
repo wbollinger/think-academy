b195810fc78d33eb34ff6d54f853cf712c878c96
cwbollinger@gmail.com <cwbollinger@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2013-03-13 01:00:41 +0000

    changes to StateGoalie
    as of now, movedir(angle, power) is broken. Don't use unless you like your code notwerken.

--- trunk/soccer2013/src/soccer/StateGoalie.java
+++ trunk/soccer2013/src/soccer/StateGoalie.java
@@ -1,5 +1,7 @@
 package soccer;
 
+import lejos.nxt.Button;
+
 public class StateGoalie extends State {
 
 	private static StateGoalie instance = new StateGoalie();
@@ -16,16 +18,16 @@ public class StateGoalie extends State {
 		double heading = r.compass.getDegrees();
 
 		r.io.debugln("" + heading);
-		while (true) {
+		while (Button.ENTER.isUp()) {
 
-			if (r.USY.getDistance() > 15) {
-				while (r.USY.getDistance() > 15) {
-					r.nav.moveDir(270, 100);
+			if (r.USY.getDistance() > 20) {
+				while (r.USY.getDistance() > 20) {
+					r.nav.moveDir(270);
 				}
 				r.floatAll();
-			} else if (r.USY.getDistance() < 10) {
-				while (r.USY.getDistance() < 10) {
-					r.nav.moveDir(90, 100);
+			} else if (r.USY.getDistance() < 15) {
+				while (r.USY.getDistance() < 15) {
+					r.nav.moveDir(90);
 				}
 				r.floatAll();
 			}
@@ -39,23 +41,15 @@ public class StateGoalie extends State {
 
 			if ((r.IR.getDirection() == 5) || (r.IR.getDirection() == 0)) {
 				r.stopAll();
-			} else if ((r.IR.getDirection() < 5) && !(r.USX.getDistance() > 121)) {
+			} else if (r.IR.getDirection() < 5) { //(r.IR.getDirection() < 5) && !(r.USX.getDistance() > 121)
 				// moves left, unless at edge of goal
-				r.nav.moveDir(180, 100);
-			} else if ((r.IR.getDirection() > 5) && !(r.USX.getDistance() < 61)) {
+				r.nav.moveDir(180);
+			} else if (r.IR.getDirection() > 5) { //(r.IR.getDirection() > 5) && !(r.USX.getDistance() < 61)
 				// moves right, unless at edge of goal
-				r.nav.moveDir(0, 100);
+				r.nav.moveDir(0);
 			}
-
-			// }
-			// if(r.USX.getDistance() < 100){
-			// r.moveForward();
-			// if(r.USX.getDistance() > 120){
-			// r.stopAll();
-			// }
-			// }
-
 		}
+		r.changeState(StateCommand.getInstance());
 
 	}
 
