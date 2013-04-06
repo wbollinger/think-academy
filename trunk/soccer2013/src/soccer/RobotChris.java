a50608749791e7b7865f9a8335ffd6ececf3597d
cwbollinger@gmail.com <cwbollinger@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2013-04-06 00:58:29 +0000

    Use light sensors to not cross out of bounds line

--- trunk/soccer2013/src/soccer/RobotChris.java
+++ trunk/soccer2013/src/soccer/RobotChris.java
@@ -1,6 +1,7 @@
 package soccer;
 
 import lejos.nxt.Button;
+import lejos.nxt.LightSensor;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
 import lejos.nxt.SensorPort;
@@ -8,21 +9,24 @@ import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.addon.IRSeekerV2;
 import lejos.nxt.addon.CompassHTSensor;
 
-
 public class RobotChris extends Robot {
+
+	public final int WHITE_VALUE = 42;
 
 	public RobotChris(String name) {
 		super(name);
 		motA = new NXTMotor(MotorPort.A);
 		motB = new NXTMotor(MotorPort.B);
 		motC = new NXTMotor(MotorPort.C);
-		
-		//IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
+
+		// IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
 		EIR = new EnhIRSeekerV2(SensorPort.S1);
 		compass = new CompassHTSensor(SensorPort.S2);
-		arduino = new ArduSoccer(SensorPort.S4);
-		//USY = new UltrasonicSensor(SensorPort.S3);
-		//USX = new UltrasonicSensor(SensorPort.S4);
+		lightLeft = new LightSensor(SensorPort.S3);
+		lightRight = new LightSensor(SensorPort.S4);
+		// arduino = new ArduSoccer(SensorPort.S4);
+		// USY = new UltrasonicSensor(SensorPort.S3);
+		// USX = new UltrasonicSensor(SensorPort.S4);
 
 		setPower(80);
 		motA.stop();
@@ -36,16 +40,30 @@ public class RobotChris extends Robot {
 	public void followBall() {
 		int dir;
 		int str;
-		
+		int left;
+		int right;
+
 		while (Button.ENTER.isUp()) {
 			EIR.update();
 			dir = EIR.getDir();
 			str = EIR.getStrength();
-			io.debugln("" + dir + ":" + str);
+			left = lightLeft.readValue();
+			right = lightRight.readValue();
+			io.debugln(""+left+":"+right);
+			//io.debugln("" + dir + ":" + str);
 			if (str > 350) {
 				stopAll();
 				forward(300);
 				return;
+			} else if((right > WHITE_VALUE) && (left > WHITE_VALUE)) {
+				nav.moveDir(270);
+				sleep(1000);
+			} else if (right > WHITE_VALUE) {
+				nav.moveDir(340);
+				sleep(200);
+			} else if (left > WHITE_VALUE) {
+				nav.moveDir(180);
+				sleep(200);
 			} else {
 
 				if (dir == 5) {
@@ -69,16 +87,16 @@ public class RobotChris extends Robot {
 		motB.backward();
 		motA.stop();
 	}
-	
+
 	public void forward(int time) {
 		moveForward();
-		
+
 		sleep(time);
 		motA.stop();
 		motB.stop();
 		motC.stop();
 	}
-	
+
 	public void moveBackward() {
 		motA.setPower(MOTOR_POWER);
 		motB.setPower(MOTOR_POWER);
@@ -111,11 +129,13 @@ public class RobotChris extends Robot {
 		motB.backward();
 		motC.backward();
 	}
+
 	public void moveArcRight() {
 		motA.setPower(MOTOR_POWER);
 		motA.forward();
 
 	}
+
 	public void moveArcLeft() {
 		motA.setPower(MOTOR_POWER);
 		motA.backward();
