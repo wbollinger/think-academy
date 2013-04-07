f6d720cb8a8dd9d1e5926918b7642ff730118aa7
boneman09@gmail.com <boneman09@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2013-04-07 21:15:00 +0000

    made a change so that we can set a side to be the enemy goal and on to be the ally pre-game

--- trunk/soccer2013/src/soccer/Navigator.java
+++ trunk/soccer2013/src/soccer/Navigator.java
@@ -7,8 +7,11 @@ import lejos.nxt.addon.CompassHTSensor;
 
 public class Navigator {
 
-	public static final float ENEMY_GOAL_HEADING = 306.0f;
-	public static final float ALLY_GOAL_HEADING = 113.0f;
+	public static final float YELLOW_GOAL_HEADING = 247.0f;
+	public static final float BLUE_GOAL_HEADING = 167.0f;
+	
+	public float ENEMY_GOAL;
+	public float ALLY_GOAL;
 
 	protected Robot bot;
 	private float facingDegree;
