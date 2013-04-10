dd4eb02aaa7d0af1e963d155ee9e6b948df426a6
boneman09@gmail.com <boneman09@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2013-04-10 21:16:22 +0000

    selective debugging added

--- trunk/soccer2013/src/soccer/BrickIO.java
+++ trunk/soccer2013/src/soccer/BrickIO.java
@@ -17,7 +17,17 @@ public class BrickIO {
 	private boolean useCommands;
 	private boolean useDebug;
 	
-	private int enabledDebug = 0xFF;
+	private int debugFlags = 0xFF;
+	/*
+	 * 0000 0001 IR data
+	 * 0000 0010 Ping data
+	 * 0000 0100 Light sensor data
+	 * 0000 1000 Compass data
+	 * 0001 0000 Zone data
+	 * 0010 0000 Unassigned
+	 * 0100 0000 Unassigned
+	 * 1000 0000 Unassigned
+	 */
 	
 	
 	public void debug(String msg) {
@@ -37,12 +47,38 @@ public class BrickIO {
 		// RConsole.print(msg);
 		// }
 	}
-
+	
 	public void debugln(String msg) {
 		if (useDebug == false)
 			return;
 		debug(msg + "\n");
 	}
+	
+	public void debug(String msg, int msgType) {
+		if (useDebug == false)
+			return;
+		if ((btc != null) && (outStream != null) && ((msgType & debugFlags) != 0x00)) {
+			try {
+				outStream.writeUTF(msg);
+				outStream.flush();
+			} catch (IOException e) {
+				LCD.drawString("Rbt IO Err", 0, 1);
+			}
+		}
+		// else {
+		// For competition, do nothing here
+		// System.out.println(msg);
+		// RConsole.print(msg);
+		// }
+	}
+	
+	public void debugln(String msg, int msgType) {
+		if (useDebug == false)
+			return;
+		debug(msg + "\n", msgType);
+	}
+
+	
 	
 	public boolean isStepMode() {
 		return stepMode;
@@ -66,5 +102,9 @@ public class BrickIO {
 
 	public void setUseDebug(boolean useDebug) {
 		this.useDebug = useDebug;
+	}
+	
+	public void setDebugFlags(int flags) {
+		debugFlags = flags;
 	}
 }
