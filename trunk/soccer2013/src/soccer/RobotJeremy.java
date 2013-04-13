0ca819ffb3df0f957936e2fec01943dd4b0db6e1
boneman09@gmail.com <boneman09@gmail.com@96ab6b27-3c35-dc6d-06e3-69895e3d0c79>
2013-04-13 01:18:27 +0000

    Made RobotChris identical to RobotJeremy and RobotJake identical to RobotTim

--- trunk/soccer2013/src/soccer/RobotJeremy.java
+++ trunk/soccer2013/src/soccer/RobotJeremy.java
@@ -1,149 +1,212 @@
 package soccer;
 
-import lejos.nxt.Button;
-import lejos.nxt.LCD;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
 import lejos.nxt.SensorPort;
-import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.addon.CompassHTSensor;
-import lejos.nxt.addon.IRSeekerV2;
+import lejos.nxt.addon.MMXMotor;
+import lejos.nxt.addon.NXTMMX;
 
 public class RobotJeremy extends Robot{
-	public UltrasonicSensor US;
+	
+	//NXTRegulatedMotor regMotA; 
+	//NXTRegulatedMotor regMotB;
+	//NXTRegulatedMotor regMotC;
 	
 	public RobotJeremy(String name) {
 		super(name);
 		
+		LightCorrection = 90;
+		
 		motA = new NXTMotor(MotorPort.A);
 		motB = new NXTMotor(MotorPort.B);
 		motC = new NXTMotor(MotorPort.C);
-		IR = new IRSeekerV2(SensorPort.S1, IRSeekerV2.Mode.AC);
+		
+		setPower(80);
+		motA.stop();
+		motB.stop();
+		motC.stop();
+		
+		//regMotA = new NXTRegulatedMotor(MotorPort.A);
+		//regMotB = new NXTRegulatedMotor(MotorPort.B);
+		//regMotC = new NXTRegulatedMotor(MotorPort.C);
+		
+		//regMotA.setAcceleration(1000);
+		//regMotB.setAcceleration(1000);
+		//regMotC.setAcceleration(1000);
+		
+		//regMotA.setSpeed(1000);
+		//regMotB.setSpeed(1000);
+		//regMotC.setSpeed(1000);
+
+		
+		EIR = new EnhIRSeekerV2(SensorPort.S1);
 		compass = new CompassHTSensor(SensorPort.S2);
-	    USY = new UltrasonicSensor(SensorPort.S3);
-	    USX = new UltrasonicSensor(SensorPort.S4);
-	    
+		mux = new NXTMMX(SensorPort.S3);
+		dribbler = new MMXMotor(mux, NXTMMX.MMX_MOTOR_1);
+		arduino = new ArduSoccer(SensorPort.S4);
 		
-		motA.setPower(50);
-		motB.setPower(50);
-		motC.setPower(50);
+		arduino.writeCommand(Robot.DISCONNECT_CAPS_SOLENOID);
+		sleep(50); // wait for the relay to open
+		arduino.writeCommand(Robot.CONNECT_CAPS_CHARGER);
+
+		r = 2.5;
+		b = 0;
+	}
+
+//	public void followBall() {
+//		int dir;
+//		int str;
+//		int left;
+//		int right;
+//
+//		while (Button.ENTER.isUp()) {
+//			EIR.update();
+//			dir = EIR.getDir();
+//			str = EIR.getStrength();
+//			left = lightLeft.readValue();
+//			right = lightRight.readValue();
+//			io.debugln("" + left + ":" + right);
+//			// io.debugln("" + dir + ":" + str);
+//			if (str > 350) {
+//				stopAll();
+//				dribbler.forward();
+//				forward(300);
+//				return;
+//			} else if ((right > WHITE_VALUE) && (left > WHITE_VALUE)) {
+//				nav.moveDir(270);
+//				sleep(1000);
+//			} else if (right > WHITE_VALUE) {
+//				nav.moveDir(340);
+//				sleep(200);
+//			} else if (left > WHITE_VALUE) {
+//				nav.moveDir(180);
+//				sleep(200);
+//			} else {
+//
+//				if (dir == 5) {
+//					moveForward();
+//				} else if (dir < 5) {
+//					turnLeft();
+//				} else if (dir > 5) {
+//					turnRight();
+//				} else {
+//					stopAll();
+//				}
+//			}
+//		}
+//	}
+	
+	@Override
+	public void fireSolenoid() {
+		arduino.writeCommand(Robot.DISCONNECT_CAPS_CHARGER);
+		sleep(200); // wait for the relay to open
+		arduino.writeCommand(Robot.CONNECT_CAPS_SOLENOID);
+		sleep(1000);
+		arduino.writeCommand(Robot.DISCONNECT_CAPS_SOLENOID);
+		sleep(200); // wait for the relay to open
+		arduino.writeCommand(Robot.CONNECT_CAPS_CHARGER);
+	}
+
+	@Override
+	public void moveForward() {
+		motA.setPower(MOTOR_POWER+aFudge);
+		motB.setPower(MOTOR_POWER+bFudge);
+		motC.setPower(MOTOR_POWER+cFudge);
+		
+		//regMotA.setSpeed(1000);
+		//regMotB.setSpeed(1000);
+		//regMotC.setSpeed(1000);
+		
+	
+		motB.backward();
+		motC.forward();
+		motA.stop();
+	}
+
+	@Override
+	public void forward(int time) {
+		moveForward();
+
+		sleep(time);
 		motA.stop();
 		motB.stop();
 		motC.stop();
 	}
 	
-	public void followBall() {
+	@Override
+	public void moveBackward() {
+		motA.setPower(MOTOR_POWER+aFudge);
+		motB.setPower(MOTOR_POWER+bFudge);
+		motC.setPower(MOTOR_POWER+cFudge);
 		
-		while (!Button.ENTER.isDown()) {
-			
-			if(IR.getDirection() > 0 && IR.getDirection() < 5){
-				if(IR.getSensorValue(3) > 200){
-					stopAll();
-				} else {
-				turnLeft();
-				}
-				if(IR.getSensorValue(3) > 200){
-					stopAll();
-				}
-			}
-			if(IR.getDirection() == 5){
-				stopAll();
-				moveForward();
-			}
-			if(IR.getDirection() > 5 && IR.getDirection() < 10){
-				turnRight();
-			}
-			
-		}
-	}
-	
-	public double pointToGoal(){
-		io.debugln("went into pointgoal");
-		double wall1dist; // pointing north
-		double wall2dist; // pointing east
-		wall1dist = USY.getDistance();
-		io.debugln("first reading");
-		wall2dist = USX.getDistance();
-		
-		io.debugln("Second reading");
-		sleep(1000);
-		double angle = Math.atan2(91-wall2dist, wall1dist);
-		angle = (angle*180)/Math.PI;
-		LCD.drawInt((int)angle, 3, 3);
-		io.debugln("Angle calculated");
-		if(angle > 0){
-			turnLeftprecise(angle);
-		} else {
-			turnRightprecise(Math.abs(angle));
-		}
-		io.debugln("turned");
-		sleep(1000);
-		
-		return Math.sqrt(Math.pow(wall1dist,2)+Math.pow(91-wall2dist,2));
-	}
-	
-	public void moveForward(){
-		
-		motC.forward();
-		motB.backward();
-		
-	}
-	
-	public void moveBackward(){
+		//regMotA.setSpeed(1000);
+		//regMotB.setSpeed(1000);
+		//regMotC.setSpeed(1000);
 		
 		motC.backward();
 		motB.forward();
-		
+		motA.stop();
 	}
 
-	public void moveWestForward(){
-		
-		motA.backward();
-		motB.forward();
-		
-	}
-	
-	public void moveEastForward(){
-		
-		motA.forward();
-		motC.backward();
-		
-	}
-	
-	public void moveEastBackward(){
-		
-		motA.forward();
-		motB.backward();
-		
-	}
-	
-	public void moveWestBackward(){
-		
-		motA.backward();
-		motC.forward();
-		
-	}
-	
-	public void stopAll(){
+	@Override
+	public void stopAll() {
 		motA.stop();
 		motB.stop();
 		motC.stop();
+		
+		//regMotA.stop();
+		//regMotB.stop();
+		//regMotC.stop();
 	}
 
-	public void turnLeft() {
-
-		motA.backward();
-		motB.backward();
-		motC.backward();
-
+	@Override
+	public void turnRight() {
+		motA.setPower(MOTOR_POWER+aFudge);
+		motB.setPower(MOTOR_POWER+bFudge);
+		motC.setPower(MOTOR_POWER+cFudge);
+		
+		motA.forward();
+		motB.forward();
+		motC.forward();
 	}
-	
-	public void turnLeftprecise(double degrees) {
+
+	@Override
+	public void turnRightPrecise(double degrees) {
+		//io.debugln("I'm a gonna turn right");
 		motA.resetTachoCount();
 		motB.resetTachoCount();
 		motC.resetTachoCount();
-		double count = 3.14*degrees;
-		while(Math.abs(motA.getTachoCount()) < count){
+		double count = 3.14 * degrees;
+		while (Math.abs(motA.getTachoCount()) < count) {
+			motA.forward();
+			motB.forward();
+			motC.forward();
+		}
+		stopAll();
+		motA.resetTachoCount();
+		motB.resetTachoCount();
+		motC.resetTachoCount();
+	}
+
+	@Override
+	public void turnLeft() {
+		motA.setPower(MOTOR_POWER+aFudge);
+		motB.setPower(MOTOR_POWER+bFudge);
+		motC.setPower(MOTOR_POWER+cFudge);
+		motA.backward();
+		motB.backward();
+		motC.backward();
+	}
+
+	@Override
+	public void turnLeftPrecise(double degrees) {
+		//io.debugln("I'm a gonna turn left");
+		motA.resetTachoCount();
+		motB.resetTachoCount();
+		motC.resetTachoCount();
+		double count = 3.14 * degrees;
+		while (Math.abs(motA.getTachoCount()) < count) {
 			motA.backward();
 			motB.backward();
 			motC.backward();
@@ -154,65 +217,32 @@ public class RobotJeremy extends Robot{
 		motC.resetTachoCount();
 	}
 
-	public void turnRight() {
-
+	@Override
+	public void moveArcRight() {
+		motA.setPower(MOTOR_POWER);
 		motA.forward();
-		motB.forward();
-		motC.forward();
 
 	}
 	
-public void turnRightprecise(double degrees) {
-	motA.resetTachoCount();
-	motB.resetTachoCount();
-	motC.resetTachoCount();
-	double count = 3.14*degrees;
-	while(Math.abs(motA.getTachoCount()) < count){
+	//@Override
+	public void moveArcRightPrecise() {
+		motA.setPower(MOTOR_POWER);
 		motA.forward();
-		motB.forward();
-		motC.forward();
+
 	}
-	stopAll();
-	motA.resetTachoCount();
-	motB.resetTachoCount();
-	motC.resetTachoCount();
+	
+	@Override
+	public void moveArcLeft() {
+		motA.setPower(MOTOR_POWER);
+		motA.backward();
+
+	}
+	
+	//@Override
+	public void moveArcLeftPrecise() {
+		motA.setPower(MOTOR_POWER);
+		motA.backward();
+
 	}
 
-public double[] getLocation(){
-	double wall1dist;
-	double wall2dist;
-	double xPos;
-	double yPos;
-	double array[] = new double[]{1,1};
-	wall1dist = US.getDistance();
-	try {
-		Thread.sleep(1000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	turnRightprecise(90);
-	try {
-		Thread.sleep(1000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	wall2dist = US.getDistance();
-	try {
-		Thread.sleep(1000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	turnLeftprecise(90);
-	
-	xPos = 182-wall2dist;
-	yPos = wall1dist;
-	array[0] = xPos;
-	array[1] = yPos;
-	return array;
-	
-}
-	
 }
